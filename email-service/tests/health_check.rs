1901597963320037d7081e6bafec5d8a7168431a
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-07-25 16:18:45 +0530

    ref: Refactor server setup to accept TcpListener

--- email-service/tests/health_check.rs
+++ email-service/tests/health_check.rs
@@ -1,11 +1,11 @@
-use email_service::run;
+use std::{ net::TcpListener};
 
 #[tokio::test]
 async fn health_check_works() {
     
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
-    let response = client.get("http://127.0.0.1:8000/health_check")
+    let response = client.get(&format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to send request");
@@ -14,7 +14,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = email_service::run().expect("failed to start server");
-    tokio::spawn(server);
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port  = listener.local_addr().unwrap().port();
+    let server = email_service::run(listener).expect("failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
